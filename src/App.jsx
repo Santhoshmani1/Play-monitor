b3ca84eaf2c2ed443d737f911a7a7e90ccc53ea2
santhosh <pitakasanthosh@gmail.com>
2024-03-22 22:03:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-2xl underline text-blue-500'>My app</h1>
+
+    <h2 className='text-center text-3xl p-2 text-yellow-500'> All the best ✨✨✨</h2>
+    </>
+  )
+}
+
+export default App
