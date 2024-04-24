4112c827baf7947ecaf4405a64af74f8c5473bd3
santhosh <pitakasanthosh@gmail.com>
2024-04-24 22:40:37 +0530

    Create Initial Home page component with Header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import Header from '../components/shared/Header';
+
+const Home = () => {
+  return (
+    <div className='mx-auto'>
+      <Header />
+    </div>
+  )
+}
+
+export default Home;
