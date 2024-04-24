d416bac4c825e4af6fdab36a56da81c93970072a
santhosh <pitakasanthosh@gmail.com>
2024-04-24 22:37:03 +0530

    Update Tailwind CSS configuration : Add Open Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}" ,"index.html",
-  ]
-  ,
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", "Ariel", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
