be67cad9ece63592ce4848d197ac59a41479c637
santhosh <pitakasanthosh@gmail.com>
2024-04-24 22:39:19 +0530

    Create Material-Icons component for resuable mui icons

---
+++ src/components/shared/materialIcons.jsx
@@ -0,0 +1,17 @@
+export function SearchBar() {
+    return (
+        <span className="material-icons font-semibold">search</span>
+    )
+}
+
+export function ArrowOutward() {
+    return (
+        <span className="material-icons font-semibold">arrow_outward</span>
+    )
+}
+
+export function PlayIcon({ style }) {
+    return (
+        <span className="material-icons font-semibold" style={ style }>play_arrow</span>
+    )
+}
