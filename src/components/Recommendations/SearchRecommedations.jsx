626f0e1bb6926f7830d2088985c691f51827a3cb
santhosh <pitakasanthosh@gmail.com>
2024-04-24 23:03:46 +0530

    feat : Add SearchRecommendations component for displaying search suggestions

---
+++ src/components/Recommendations/SearchRecommedations.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { SearchBar, ArrowOutward } from '../shared/materialIcons';
+
+const SearchRecommendations = ({ searchTerm }) => {
+    const [suggestions, setSuggestions] = useState(["whatsapp"]);
+    const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState(0);
+
+    // Fetch search suggestions from the API and update the state with the suggestions
+    // when the search term changes or the component mounts
+    useEffect(() => {
+        fetch("https://gplayapi.vercel.app/suggest?term=" + searchTerm)
+            .then(response => response.json())
+            .then(data => {
+                if (Array.isArray(data) && data.length >= 4) {
+                    setSuggestions(data);
+                }
+            })
+            .catch(error => console.log(error));
+    }, [searchTerm]);
+
+    // Add event listener to handle keyboard navigation through the suggestions
+    // when the selected suggestion index or the suggestions array changes
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.key === 'ArrowDown' && selectedSuggestionIndex < suggestions.length - 1) {
+                setSelectedSuggestionIndex(selectedSuggestionIndex + 1);
+            } else if (e.key === 'ArrowUp' && selectedSuggestionIndex > 0) {
+                setSelectedSuggestionIndex(selectedSuggestionIndex - 1);
+            }
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [selectedSuggestionIndex, suggestions.length]);
+
+   // Render the search suggestions with the selected suggestion highlighted 
+   // based on the selectedSuggestionIndex state value 
+    return (
+        <div className='flex justify-center items-center flex-col ml-6 mt-1 font-sans'>
+            {suggestions.map((suggestion, index) => (
+                    <div key={index} className={`py-2 w-3/5 lg:w-2/5 mr-14 bg-zinc-50 flex text-lg items-center justify-between pr-4 ${index === selectedSuggestionIndex ? 'bg-zinc-400 text-blue-400 font-bold ' : ''}`}>
+                        <div className='flex'>
+                            <div className='px-2'>
+                                <SearchBar />
+                            </div>
+                            <div>
+                                {suggestion}
+                            </div>
+                        </div>
+                        <div>
+                            <ArrowOutward />
+                        </div>
+                    </div>
+            ))}
+        </div>
+    );
+}
+
+export default SearchRecommendations;
