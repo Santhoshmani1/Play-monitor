487f77bd0f6bb8705bc085bf3896b14097570385
santhosh <pitakasanthosh@gmail.com>
2024-04-27 14:57:59 +0530

    fix : Removed  sign-in button in Header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -4,15 +4,10 @@ import { PlayIcon } from './materialIcons'
 const Header = () => {
     return (
         <>
-            <header className='p-2 lg:p-4 bg-black flex justify-between items-center'>
+            <header className='p-2 lg:p-3 bg-black flex justify-between items-center'>
                 <div className='flex'>
                     <PlayIcon style={{ fontSize: "40px", color: "#069aed" }} />
                     <h1 className='text-white font-bold text-3xl font-sans'>Play Monitor </h1>
-                </div>
-                <div className="call-to-action-btn">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded">
-                        Sign In
-                    </button>
                 </div>
             </header>
         </>
