2e572747effa34ea876c7940a8e20d2380015591
santhosh <pitakasanthosh@gmail.com>
2024-04-27 19:59:04 +0530

    feat: Add text to text response with gemini AI model
    Configure model generateConfig, safetySettings and system Instructions

--- googleAI-api/api/index.js
+++ googleAI-api/api/index.js
@@ -16,6 +16,61 @@ const API_KEY = process.env.GOOGLEAI_API_KEY;
 app.use(express.json());
 app.use(cors());
 
+async function runChat(userInput) {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 1,
+    topK: 0,
+    topP: 0.95,
+    maxOutputTokens: 8192,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chatInstructions = {
+    role: "model",
+    parts: [
+      {
+        text: `Divide the google play store mobile application reviews into the following prescribed format ->  
+        - # **Issue category** - design/bug-fix/feature-request
+        -  Title : ** ## Issue - Title ** Appropriate Title for the issue
+        - ID : id of the review
+    `,
+      },
+    ],
+  };
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+    systemInstruction: chatInstructions,
+  });
+
+  const result = await chat.sendMessage(userInput);
+  const response = result.response;
+  return response.text();
+}
+
 
 app.listen(3000, () => {
   console.log(`Google AI API is listening on PORT :${port} `);
