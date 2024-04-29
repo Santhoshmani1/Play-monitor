d6974b9ba19c1da334b989d5109389c678cd8848
santhosh <pitakasanthosh@gmail.com>
2024-04-29 18:13:43 +0530

    feat: Add common export for assets

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import playStoreIcon from './playstorelogo.avif'
+import discoverPreview from './discoverPreview.png'
+import searchPreview from './searchPreview.png'
+import similar from './similar.png'
+
+export { playStoreIcon, searchPreview, discoverPreview, similar };
