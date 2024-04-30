42e54c9b0e7410140b3bb9269f769bf10b0ca9e9
santhosh <pitakasanthosh@gmail.com>
2024-04-30 18:21:21 +0530

    fix: Make searchbar icon as clickable Link element

--- src/components/search/EntrySearch.jsx
+++ src/components/search/EntrySearch.jsx
@@ -1,6 +1,7 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 import { SearchBar } from '../shared/materialIcons';
 import SearchRecommendations from '../Recommendations/SearchRecommedations';
+import { Link } from 'react-router-dom';
 
 const EntrySearch = () => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -8,13 +9,12 @@ const EntrySearch = () => {
     return (
         <>
             <div className="flex items-center justify-center py-4 searchbar">
-                <input type="text" placeholder='Search Apps & Games' className='w-3/5 px-2 py-3 text-gray-700 border-2 border-black rounded-3xl lg:w-2/5 lg:text-lg md:text-xl focus:border-gray-950' onChange={(e) => {
+                <input type="text" placeholder='Search Apps & Games' className='w-3/5 p-3 text-gray-700 border-2 border-black rounded-3xl lg:w-2/5 lg:text-lg md:text-xl focus:border-blue-950' onChange={(e) => {
                     setSearchTerm(e.target.value)
-                }} onFocusCapture={() => {
                 }}
                     autoFocus={true}
                 />
-                <SearchBar />
+                <Link to={"/apps/"+searchTerm} >  <SearchBar /></Link>
             </div>
 
             {/* Render the search recommendations component when the search term is not empty */}
